78d7143b3e266e3f6f01b4d0916c08d636308d32
Zachary Temkin <zachtemkin@gmail.com>
2022-09-07 16:46:25 -0700

    stores and updates value for query on the server so that each client doesn not cause a unique call to the twitter api

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-const cors = require("cors");
 const express = require("express");
-const dotenv = require("dotenv");
+const cors = require("cors");
 const needle = require("needle");
+const mcache = require("memory-cache");
+const dotenv = require("dotenv");
 
 const app = express();
-
 app.use(cors()).options("*", cors());
 
 dotenv.config();
 const BearerToken = process.env.BEARER_TOKEN;
+
+const cache = (duration) => {
+  return (req, res, next) => {
+    let key = "__express__" + req.originalUrl || req.url;
+    let cachedBody = mcache.get(key);
+
+    if (cachedBody) {
+      res.send(cachedBody);
+      return;
+    } else {
+      res.sendResponse = res.send;
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body);
+      };
+      next();
+    }
+  };
+};
 
 const endpointUrl = "https://api.twitter.com/2/tweets/counts/recent";
 
@@ -45,12 +64,12 @@ const getTweets = async (queryString) => {
   }
 };
 
-const getTweetCount = async (req, res) => {
+const getTweetCount = async (query) => {
   try {
-    let twitterData = await getTweets(req.query.queryString);
-    res.send(await analyze(twitterData));
+    let twitterData = await getTweets(query);
+    return await analyze(twitterData);
   } catch (error) {
-    res.send(error);
+    console.log(error);
   }
 };
 
@@ -58,7 +77,39 @@ const analyze = async (twitterData) => {
   return twitterData.meta;
 };
 
-app.get("/", getTweetCount);
+// test counter --------------------------------------------
+
+const counters = [];
+
+setInterval(() => {
+  counters.map(async (counter) => {
+    const newCount = await getTweetCount(counter.query);
+    counter.count = newCount;
+  });
+}, 7000);
+
+const getTestCount = async (req, res) => {
+  const queryVal = req.query.queryString;
+
+  if (!counters.find((e) => e.query === queryVal)) {
+    const newCount = await getTweetCount(queryVal);
+
+    const newCounter = {
+      query: queryVal,
+      count: newCount,
+    };
+
+    counters.push(newCounter);
+    console.log(`just added ${newCounter.query}`);
+  }
+
+  const currentCounter =
+    counters[counters.findIndex((e) => e.query === queryVal)];
+
+  res.send(currentCounter.count);
+};
+
+app.get("/", getTestCount);
 
 app.listen(process.env.PORT || 3001, () => {
   console.log(`Listening on port ${process.env.PORT}`);
